31c3e759d671be40b8919a781f577f9727ad5ce8
Sinisa Laptop <sinisahamburg@hotmail.de>
2016-11-10 20:01:19 +0100

    [ADDED]PlayerMovement added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float spaceToCam = 5f;
+    public float speed = 0.1f;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frameCamera.main.ScreenToWorldPoint(temp)
+    void Update () {
+
+        Vector3 temp = Input.mousePosition;
+        temp.z = spaceToCam;
+        Vector3 temp2 = Camera.main.ScreenToWorldPoint(temp);
+
+        this.transform.Translate(0, 0, temp2.z * speed * Time.deltaTime);
+	}
+}
